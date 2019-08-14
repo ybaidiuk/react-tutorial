5e677ef09b2ac32bec3da05bcfa30adfcd5dde38
Yevhen Baidiuk <y.baidiuk@gmail.com>
2019-08-14 12:34:46 +0300

    add Material-UI lib

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 /** @jsx jsx */
 import {css, jsx} from '@emotion/core'
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div
       css={css`
+        height: 900px;
         background-color: hotpink;
       `}>
-      Hello World
+      <Button variant='contained' color='primary'>
+        Primary
+      </Button>
     </div>
   )
 }
